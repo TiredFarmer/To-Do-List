4afd0295542efa94fa86df20e35e0b5593cb7bb0
cadenlu121 <96642163+cadenlu121@users.noreply.github.com>
2022-02-19 15:20:51 -0800

    Update get_quote.py
    hi

--- get_quote.py
+++ get_quote.py
@@ -1,5 +1,11 @@
-def get_quote() -> str:
-    """
-    Gets quote from api and return string of that quote
-    """
-    pass
+import requests
+import json
+
+def get_quote():
+  """
+  gets a random quote from the zenquote api (motivation!!!)
+  """
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + "\r\n - " + json_data[0]['a']
+  return quote
